49e644a9f9f76a60fddc19e1501202210dbe4a78
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2025-07-07 23:22:03 +0700

    web profil sementara

--- src/components/element/Paragraph.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement, ReactNode } from "react";
-
-export default function Paragraph({
-  children,
-}: {
-  children: ReactNode;
-}): ReactElement {
-  return <p>{children}</p>;
-}
